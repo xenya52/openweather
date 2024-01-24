575f4cedfecee40dce510d3894b4b87ce2d8cc3a
xenya52 <108221381+xenya52@users.noreply.github.com>
2024-01-24 14:09:46 +0100

    format output with a json reader

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,21 @@
+use serde_json::Value;
 use dotenv::dotenv;
 use std::{env, error::Error};
 use reqwest;
 use tokio;
+
+fn print_weather_output(body: String) -> Result<(), serde_json::Error> {
+    // Parse the string of data into serde_json::Value.
+    let v: Value = serde_json::from_str(&body)?;
+
+    // Access parts of the data by indexing with square brackets.
+    let name = v["name"].as_str().unwrap_or("unknown");
+    let weather = v["weather"][0]["main"].as_str().unwrap_or("unknown");
+    let temperature = v["main"]["temp"].as_f64().unwrap_or_default();
+
+    println!("Weather in [{}] is [{}]. Temperature is [{:.1}°C]", name, weather, temperature);
+    Ok(())
+}
 
 #[tokio::main]
 async fn main() -> Result<(), reqwest::Error> {
@@ -18,7 +32,8 @@ async fn main() -> Result<(), reqwest::Error> {
     if response.status().is_success() {
         // Parse the response text and print it
         let body = response.text().await?;
-        println!("Response Text: {}", body);
+        //println!("Response Text: {}", body); debug reasons
+        print_weather_output(body);
     } else {
         // Handle the error case
         println!("Request failed with status: {}", response.status());
