4a5191074585073f3fa40dd5f187d5ac9b0de096
xenya52 <108221381+xenya52@users.noreply.github.com>
2024-01-30 12:39:19 +0100

    try to give key with args

--- src/main.rs
+++ src/main.rs
@@ -123,9 +123,10 @@ fn print_weather_output(body: String) -> Result<(), serde_json::Error> {
 
 #[tokio::main]
 async fn main() -> Result<(), reqwest::Error> {
+    let args: Vec<String> = env::args().collect();
     dotenv().ok();
     // The URL of the API you want to call
-    let api_key = env::var("OPENWEATHER_KEY").expect("Key not found"); // Replace with your actual API key
+    let api_key = &args[1]; // Replace with your actual API key
     let city = "Regensburg";
     let url = format!("http://api.openweathermap.org/data/2.5/weather?q={}&appid={}&units=metric", city, api_key);
 
